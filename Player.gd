f7f01d6a0295c486e6130ab376e1c8940816412f
dguia18 <dguia@unicesar.edu.co>
2019-12-02 07:32:08 -0500

    inclusion de botones

--- Player.gd
+++ Player.gd
@@ -3,11 +3,12 @@ export (int) var velocidad
 var movimiento = Vector2()
 var limite
 signal golpe
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
+var upDirection = false
+var downDirection = false
+var rigthDirection = false
+var leftDirection = false
+
 func _ready():
 	#hide()
 	limite = get_viewport_rect().size
@@ -15,13 +16,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	movimiento = Vector2()
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right") or rigthDirection:
 		movimiento.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") or leftDirection:
 		movimiento.x -=1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") or upDirection:
 		movimiento.y -=1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down") or downDirection:
 		movimiento.y +=1
 	
 	if movimiento.length() >  0:
@@ -37,6 +38,8 @@ func _process(delta):
 		$AnimatedSprite.flip_h = movimiento.y > 0		
 	else:
 		$AnimatedSprite.animation = "frente"
+		
+
 
 func _on_Player_body_entered(body):
 	hide()
@@ -47,3 +50,34 @@ func Inicio(posicion):
 	position = posicion
 	show()
 	$collisionPlayer.disabled = false
+	
+
+func directionDown():
+	downDirection = true
+	
+
+func directionUp():
+	upDirection = true
+	print("llego a up")
+	
+
+func directionLeft():
+	leftDirection = true
+	
+
+func directionRigth():
+	rigthDirection = true
+	print("eeror")
+
+func _on_interfaz_noleft():
+	leftDirection = false
+	
+func _on_interfaz_noup():
+	upDirection = false
+
+func _on_interfaz_nodown():
+	downDirection = false
+	print("downDirecion: ",downDirection)
+
+func _on_interfaz_norigth():
+	rigthDirection = false
--- interfaz.gd
+++ interfaz.gd
@@ -1,6 +1,14 @@
 extends CanvasLayer
 
 signal iniciarJuego
+signal down
+signal up
+signal left
+signal rigth
+signal noleft
+signal nodown
+signal noup
+signal norigth
 
 func mostrarMensaje(texto):
 	$lblMensaje.text = texto
@@ -26,3 +34,35 @@ func _on_mensajeTimer_timeout():
 func _on_btnPlay_pressed():
 	$btnPlay.hide()
 	emit_signal("iniciarJuego")
+
+
+func _on_btnLeft_pressed():
+	emit_signal("left")
+
+
+func _on_btnUp_pressed():
+	emit_signal("up")
+
+
+func _on_btnRight_pressed():
+	emit_signal("rigth")
+
+
+func _on_btnDown_pressed():
+	emit_signal("down")
+
+
+func _on_btnLeft_button_up():
+	emit_signal("noleft")
+
+
+func _on_btnRigth_button_up():
+	emit_signal("norigth")
+
+
+func _on_btnUp_button_up():
+	emit_signal("noup")
+
+
+func _on_btnDown_button_up():
+	emit_signal("nodown")
